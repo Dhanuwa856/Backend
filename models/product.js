d08b457f849dbd5d361478496bb1fffe8fd547f3
Dhanushka Rathnayaka <pdhanushka856@gmail.com>
2024-10-20 16:55:05 +0530

    Create productSchema

--- models/product.js
+++ models/product.js
@@ -1,3 +1,24 @@
 import mongoose from "mongoose";
+import AutoIncrementFactory from "mongoose-sequence";
 
-const productSchema = new mongoose.Schema({});
+const AutoIncrement = AutoIncrementFactory(mongoose.connection);
+
+const productSchema = new mongoose.Schema({
+  product_id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: { type: String, required: true },
+  price: { type: String, required: true },
+  url: { type: String, required: true },
+  image_url: { type: String, required: true },
+  condition: { type: String },
+  rating: { type: String },
+  location: { type: String },
+});
+
+productSchema.plugin(AutoIncrement, { inc_field: "product_id" });
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
